fff1ccebbf088a074a2bdce04e641c5d77ff7823
jorgemanzo <jmanzo203689@gmail.com>
2020-02-12 17:27:13 -0800

    Query working using admin secret

--- pages/app/account.js
+++ pages/app/account.js
@@ -1,20 +1,42 @@
 import React from 'react'
 import { withAuth, withLoginRequired, useAuth } from 'use-auth0-hooks'
-
+import { useQuery } from 'urql'
 import AppHeader from '../../components/app_header'
-
+const getUsers = `
+query getUser {
+  Users {
+    email
+  }
+}
+`
 const Account = () => {
 
-  const res = useAuth({
-    audience: '',
-    scope: ''
-  });
+  const res = useAuth({});
 
-  console.log(res);
+  const [result] = useQuery({
+      query: getUsers
+  })
+  console.log(result)
 
+  // This could be cleaned up later
+  let message;
+  if(result.fetching) {
+        message = 'Fetching ...'
+  } else if(!result.fetching) {
+        if(result.error) {
+                message = 'Error in fetching. Check browser console'
+        } else if(result.data) {
+                if(result.data.Users.length > 0) {
+                        message = 'Your email is: ' + result.data.Users[0].email
+                } else {
+                        message = 'No users found in database'
+                }
+        }
+  }
   return (
     <div>
       <AppHeader header={[{name: 'Account', dest: '/app/account'}]}/>
+      <p>{message}</p>
     </div>
   )
 }
