0dd991f3dc572b3b85db7f37cde7a538401105df
jorgemanzo <jmanzo203689@gmail.com>
2020-02-13 10:00:51 -0800

    Applied requested changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,6 @@ import { Auth0Provider } from 'use-auth0-hooks'
 import UrqlProvider from '../components/urqlProvider';
 import auth0 from '../utils/auth0';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-import { ThemeProvider } from 'emotion-theming'
-import theme from '../theme'
 import Nav from '../components/nav'
 
 
@@ -17,8 +15,7 @@ export default class extends App {
             domain={process.env.AUTH0_DOMAIN}
             clientId={process.env.AUTH0_CLIENT_ID}
             redirectUri={process.env.REDIRECT_URI}>
-            <UrqlProvider nested={<Component {...pageProps}/>} >
-            </UrqlProvider >
+            <UrqlProvider nested={<Component {...pageProps}/>} />
       </Auth0Provider>
     )
   }
