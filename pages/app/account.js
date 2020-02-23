c345cd5f427fc2684b15733f790e771cacdea82f
jorgemanzo <jmanzo203689@gmail.com>
2020-02-23 14:42:23 -0800

    Added comments to accounts page

--- pages/app/account.js
+++ pages/app/account.js
@@ -18,11 +18,20 @@ mutation AddUser($email: String!) {
     }
   }}
 `
-const checkAndInsert = async (auth, queryResult, setMessage, setUser, mutationResult, executeMutation) => {
+
+// auth: Has data of who is logged in
+// queryResult: We ran a query to see if the user was in the Users table by querying for the email address
+// setMessage: Lets us set the message in the UI.
+// setUser: Lets us save the user's data on the component
+// mutationResult: Upon calling executeMutation, mutationResult is populated with the mutation result
+// executeMutation: calling this function will insert the user into the database
+const checkAndInsert = (auth, queryResult, setMessage, setUser, mutationResult, executeMutation) => {
+    // Check to see if our query tells us the user has a user record
     if(!queryResult.fetching && !queryResult.error && queryResult.data.Users.length > 0) {
         console.log("Your Id is: ", queryResult.data.Users[0].Id)
         setMessage(`Welcome back ${auth.user.email}, your Id is ${queryResult.data.Users[0].Id}.`)
         setUser(queryResult.data.Users[0])
+    // Check to see if the query tells us the user needs a new record created
     } else if(!queryResult.fetching && !queryResult.error && queryResult.data.Users.length === 0) {
         console.log("You must be new here!")
         executeMutation({ email : auth.user.email}).then(mutationResult => {
@@ -30,6 +39,7 @@ const checkAndInsert = async (auth, queryResult, setMessage, setUser, mutationRe
             setUser(mutationResult.data.insert_Users.returning[0])
             setMessage(`Welcome ${auth.user.email}! Your Id is ${mutationResult.data.insert_Users.returning[0].Id}.`)
         })
+    // Something horrible happened.
     } else {
         console.log('Something weird happened: ', queryResult)
         setMessage( `Something weird happened. Check the dev console.`)
@@ -46,15 +56,13 @@ const Account = () => {
     // Setup a mutation to add the dummy user
     const [mutationResult, executeMutation] = useMutation(addUserMutation);
 
-    let showCreateButton = false
-
-
-
     // Check is user is already in the database
     const [queryResult] = useQuery({
         query: getUserId,
         variables: getUserIdVariables(auth.user.email)
     })
+
+    // useEffect() will call the given function if queryResult changes. This presents the inifinite redraw loop
     useEffect(() => {
         checkAndInsert(auth, queryResult, setMessage, setUser, mutationResult, executeMutation)
     }, [queryResult])
