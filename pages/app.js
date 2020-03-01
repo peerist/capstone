ef38b22904593c530e0d772f867b7e100f6e7463
jorgemanzo <jmanzo203689@gmail.com>
2020-03-01 11:13:59 -0800

    Moved account creation jit to dashboard page

--- pages/app.js
+++ pages/app.js
@@ -1,5 +1,7 @@
-import React from 'react'
-import { withAuth, withLoginRequired } from 'use-auth0-hooks'
+import React, { useEffect } from 'react'
+import { withAuth, withLoginRequired, useAuth } from 'use-auth0-hooks'
+import { useQuery, useMutation } from 'urql'
+import { getUserId, addUser } from './queries'
 import Link from 'next/link'
 import styled from '@emotion/styled'
 
@@ -19,29 +21,68 @@ const DashBoardLink = styled.a`
   padding: 20px;
   width: 33%;
 `
+// auth: Has data of who is logged in
+// queryResult: We ran a query to see if the user was in the Users table by querying for the email address
+// setMessage: Lets us set the message in the UI.
+// setUser: Lets us save the user's data on the component
+// mutationResult: Upon calling executeMutation, mutationResult is populated with the mutation result
+// executeMutation: calling this function will insert the user into the database
+const checkAndInsert = (auth, queryResult, mutationResult, executeMutation) => {
+    // Check to see if our query tells us the user has a user record
+    if(!queryResult.fetching && !queryResult.error && queryResult.data.Users.length > 0) {
+        console.log("Your Id is: ", queryResult.data.Users[0].Id)
+        // Check to see if the query tells us the user needs a new record created
+    } else if(!queryResult.fetching && !queryResult.error && queryResult.data.Users.length === 0) {
+        console.log("You must be new here!")
+        executeMutation({ email : auth.user.email}).then(mutationResult => {
+            console.log(mutationResult)
+            console.log(`Welcome ${auth.user.email}! Your Id is ${mutationResult.data.insert_Users.returning[0].Id}.`)
+        })
+        // Something horrible happened.
+    } else {
+        console.log('Something weird happened: ', queryResult)
+    }
 
+
+
+}
 const App = () => {
-  return (
-    <div>
-      <AppHeader header={[{name: 'Dashboard', dest: '/app'}]}/>
+    // get login information
+    const auth = useAuth({});
 
-      <Container pt={3} justifyContent='space-between'>
-        <Link href='/app/segments' passHref>
-          <DashBoardLink>Segments</DashBoardLink>
-        </Link>
-        <Link href='/app/papers' passHref>
-          <DashBoardLink>Papers</DashBoardLink>
-        </Link>
-        <Link href='/app/circles' passHref>
-          <DashBoardLink>Circles</DashBoardLink>
-        </Link>
-      </Container>
+    // Check is user is already in the database
+    const [queryResult] = useQuery({
+        query: getUserId,
+        variables: {email: auth.user.email }
+    })
 
-      <Container pt={3}>
-        <Divider />
-      </Container>
-    </div>
-  )
+    // Setup a mutation to add the dummy user
+    const [mutationResult, executeMutation] = useMutation(addUser);
+    // useEffect() will call the given function if queryResult changes. This prevents the inifinite redraw loop
+    useEffect(() => {
+        checkAndInsert(auth, queryResult, mutationResult, executeMutation)
+    }, [queryResult])
+    return (
+        <div>
+            <AppHeader header={[{name: 'Dashboard', dest: '/app'}]}/>
+
+            <Container pt={3} justifyContent='space-between'>
+                <Link href='/app/segments' passHref>
+                    <DashBoardLink>Segments</DashBoardLink>
+                </Link>
+                <Link href='/app/papers' passHref>
+                    <DashBoardLink>Papers</DashBoardLink>
+                </Link>
+                <Link href='/app/circles' passHref>
+                    <DashBoardLink>Circles</DashBoardLink>
+                </Link>
+            </Container>
+
+            <Container pt={3}>
+                <Divider />
+            </Container>
+        </div>
+    )
 }
 
 export default withLoginRequired(withAuth(App))
--- pages/app/segments/create.js
+++ pages/app/segments/create.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Text, Box, Button } from 'rebass'
 import { Label, Input, Textarea } from '@rebass/forms'
 import { withAuth, withLoginRequired, useAuth } from 'use-auth0-hooks'
@@ -34,54 +34,61 @@ const ConfirmButton = styled(Button)`
   }
 `;
 
-const CreateSegment = () => {
-  const auth = useAuth({});
-  const [mutationResult, executeMutation] = useMutation(addSegment);
-  const click_confirm = () => {
-    console.log("Segment created");
-    Router.push('/app/segments');
-  }
+const CreateSegment = () => { const auth = useAuth({});
+    const [segmentTitle, setSegmentTitle] = useState('')
+    const [segmentContent, setSegmentContent] = useState('')
+    const [mutationResult, executeMutation] = useMutation(addSegment);
+    const click_confirm = () => {
+        console.log("Segment created");
+        Router.push('/app/segments');
+    }
 
-  // Call this to insert a segment, ideally on a form submit
-  const createNewSegment = (segmentName, content) => {
-    executeMutation({ segmentName: segmentName, email: auth.user.email, content: content }).then(mutationResult => {
-        console.log(mutationResult.data.insert_Segment.returning[0])
-        click_confirm()
-    })
-  }
+    // Call this to insert a segment, ideally on a form submit
+    const createNewSegment = (segmentTitle, segmentContent) => {
+        executeMutation({ segmentName: segmentTitle, email: auth.user.email, content: segmentContent }).then(mutationResult => {
+            console.log(mutationResult.data.insert_Segment.returning[0])
+            click_confirm()
+        })
+    }
+    const handleConfirmClick = () => {
+        console.log(segmentTitle, segmentContent)
+        createNewSegment(segmentTitle, segmentContent)
+    }
 
-  const click_confirm = () => {
-    // We need to insert into DB first. How to get form data?
-    // createNewSegment(event.target[0].value, event.target[1].value)
-  }
-  return (
-    <div>
-      <AppHeader header={[{name: 'Dashboard', dest: '/app'}, {name: 'Segments', dest: '/app/segments'}, {name: 'Create Segment', dest: '/app/segments/create'}]}/>
+    const handleTitleChange = (event) => {
+        setSegmentTitle(event.target.value)
+    }
+    const handleContentChange = (event) => {
+        setSegmentContent(event.target.value)
+    }
+    return (
+        <div>
+            <AppHeader header={[{name: 'Dashboard', dest: '/app'}, {name: 'Segments', dest: '/app/segments'}, {name: 'Create Segment', dest: '/app/segments/create'}]}/>
 
-      <Container pt={3} justifyContent='space-between'>
-        <Text variant='heading' style={{padding: '11px 0px 10px 20px'}}>
-          Create Segment
-        </Text>
-      </Container>
+            <Container pt={3} justifyContent='space-between'>
+                <Text variant='heading' style={{padding: '11px 0px 10px 20px'}}>
+                    Create Segment
+                </Text>
+            </Container>
 
-      <Container pt={3}>
-        <Divider />
-      </Container>
+            <Container pt={3}>
+                <Divider />
+            </Container>
 
-      <Container pt='4' pl='5' pr='5' pb='5'>
-        <Box width={1} mb='3'>
-          <Label htmlFor='title' style={{fontWeight: 'bold'}} mb='1'>Title</Label>
-          <Input id='title' name='title' placeholder='Example Title' mb='3'/>
-          <Label htmlFor='content' style={{fontWeight: 'bold'}} mb='1'>Content</Label>
-          <Textarea id='content' name='content' placeholder='Example Content' style={{height: '250px'}} mb='2'/>
-          <ConfirmButton onClick={click_confirm}>
-            <FontAwesomeIcon icon={faCheckCircle} />
-            Confirm
-          </ConfirmButton>
-        </Box>
-      </Container>
-    </div>
-  )
+            <Container pt='4' pl='5' pr='5' pb='5'>
+                <Box width={1} mb='3'>
+                    <Label htmlFor='title' style={{fontWeight: 'bold'}} mb='1'>Title</Label>
+                    <Input id='title' name='title' placeholder='Example Title' mb='3' onChange={handleTitleChange} value={segmentTitle}/>
+                    <Label htmlFor='content' style={{fontWeight: 'bold'}} mb='1'>Content</Label>
+                    <Textarea id='content' name='content' placeholder='Example Content' style={{height: '250px'}} mb='2' onChange={handleContentChange} value={segmentContent}/>
+                    <ConfirmButton onClick={ handleConfirmClick }>
+                        <FontAwesomeIcon icon={faCheckCircle} />
+                        Confirm
+                    </ConfirmButton>
+                </Box>
+            </Container>
+        </div>
+    )
 }
 
 export default withLoginRequired(withAuth(CreateSegment))
