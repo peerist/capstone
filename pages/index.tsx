c5d4e6f72d6a4fe40e5d2b74a9eae3e6f78940f1
hoskinsc <32206171+hoskinsc@users.noreply.github.com>
2020-03-02 21:40:36 -0800

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import Router from 'next/router'
-import { useAuth } from 'use-auth0-hooks'
-import MainHeader from '../components/main_header'
-import MainBody from '../components/main_body'
+  import React from 'react'
+  import Router from 'next/router'
+  import { useAuth } from 'use-auth0-hooks'
+  import MainHeader from '../components/main_header'
+  import MainBody from '../components/main_body'
 
-const Home = () => {
-  const { isAuthenticated } = useAuth();
+  const Home = () => {
+    const { isAuthenticated } = useAuth();
 
-  if (isAuthenticated) {
-    Router.push('/app')
+    if (isAuthenticated) {
+      Router.push('/app')
+    }
+
+    return (
+      <div>
+        <MainHeader header={['Papers.']}/>
+        <MainBody />
+      </div>
+
+    )
   }
 
-  return (
-    <div>
-      <MainHeader header={['Papers.']}/>
-      <MainBody />
-    </div>
-
-  )
-}
-
-export default Home
+  export default Home
