06f1b48bd99e3623b0e7936d29da8a4e0b50e039
hoskinsc <32206171+hoskinsc@users.noreply.github.com>
2020-03-02 21:41:04 -0800

    Update main_header.tsx

--- components/main_header.tsx
+++ components/main_header.tsx
@@ -1,35 +1,35 @@
-  import React, { FC } from 'react'
-  import { Flex, Text, Box } from 'rebass'
+import React, { FC } from 'react'
+import { Flex, Text, Box } from 'rebass'
 
-  interface MainHeaderProps {
-    header: string[]
-  }
+interface MainHeaderProps {
+  header: string[]
+}
 
 
-  const MainHeader: FC<MainHeaderProps> = props => (
-    <Flex justifyContent='center' css={{alignItems: 'center', width: '100%', background: 'black', height: '20em' }}>
-      <Box p={5} css={{ WebkitFlexBasis:'1140px', width: '100%'}}>
-        <Text variant='main_header'>
-          {props.header.length === 1
-            ? props.header[0]
-            : (
-              <div>
-                {props.header.slice(0, props.header.length - 1).map(item => {
-                  return (
-                    <span style={{marginRight: '2em'}}>
-                      {item}
-                    </span>
-                  )
-                })}
+const MainHeader: FC<MainHeaderProps> = props => (
+  <Flex justifyContent='center' css={{alignItems: 'center', width: '100%', background: 'black', height: '20em' }}>
+    <Box p={5} css={{ WebkitFlexBasis:'1140px', width: '100%'}}>
+      <Text variant='main_header'>
+        {props.header.length === 1
+          ? props.header[0]
+          : (
+            <div>
+              {props.header.slice(0, props.header.length - 1).map(item => {
+                return (
+                  <span style={{marginRight: '2em'}}>
+                    {item}
+                  </span>
+                )
+              })}
 
-              </div>
-            )
-          }
-          <br></br>
-          For everyone.
-        </Text>
-      </Box>
-    </Flex>
-  )
+            </div>
+          )
+        }
+        <br></br>
+        For everyone.
+      </Text>
+    </Box>
+  </Flex>
+)
 
-  export default MainHeader
+export default MainHeader
