d91695c0af0632d87da44bd0d775b657991bb86a
jorgemanzo <jmanzo203689@gmail.com>
2020-03-07 01:12:14 -0800

    brought back urql provider #15

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from 'emotion-theming'
 import App from 'next/app'
 import { Auth0Provider } from 'use-auth0-hooks'
-
+import UrqlProvider from '../components/urqlProvider';
 import auth0 from '../utils/auth0';
 
 import theme from '../theme'
@@ -14,17 +14,12 @@ export default class extends App {
     const { Component, pageProps } = this.props;
 
     return (
-
       <Auth0Provider
         domain={process.env.AUTH0_DOMAIN}
         clientId={process.env.AUTH0_CLIENT_ID}
         redirectUri={process.env.REDIRECT_URI}>
-          <ThemeProvider theme={theme}>
-            <Nav />
-            <Component {...pageProps}/>
-          </ThemeProvider>
+        <UrqlProvider nested={<Component {...pageProps}/>} />
       </Auth0Provider>
-
     )
   }
 }
