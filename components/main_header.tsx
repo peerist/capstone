cbb24767b160fdebd654b993e6b8a9da7d6a80d1
hoskinsc <32206171+hoskinsc@users.noreply.github.com>
2020-03-14 14:53:12 -0700

    Fixed Front Page

--- components/main_header.tsx
+++ components/main_header.tsx
@@ -1,32 +1,26 @@
 import React, { FC } from 'react'
+import TextLoop from "react-text-loop";
 import { Flex, Text, Box } from 'rebass'
-import Typing from 'react-typing-animation'
-
 
 const MainHeader: FC<{}> = props => (
   <Flex justifyContent='center' css={{alignItems: 'center', width: '100%', background: 'black', height: '20em' }}>
     <Box p={5} css={{ WebkitFlexBasis:'1140px', width: '100%'}}>
       <Text variant='main_header'>
             <div>
-              <Typing>
-                  <span>Papers.</span>
-                  <Typing.Backspace count={7} delay={2000} />
-                  <span>Peer reviews.</span>
-                  <Typing.Backspace count={14} delay={2000} />
-                  <span>Experiments.</span>
-                  <Typing.Backspace count={12} delay={2000} />
-                  <span>Datasets.</span>
-                  <Typing.Backspace count={9} delay={2000} />
-                  <span>Citations.</span>
-                  <Typing.Backspace count={10} delay={2000} />
-                  <span>Science.</span>
-                  <Typing.Backspace count={8} delay={2000} />
-              </Typing>
+              <TextLoop interval={5000}>
+                  <span>Papers</span>
+                  <span>Peer Reviews</span>
+                  <span>Experiments</span>
+                  <span>Datasets</span>
+                  <span>Citations</span>
+                  <span>Science</span>
+              </TextLoop>{" "}
             </div>
         For everyone.
       </Text>
     </Box>
   </Flex>
+
 )
 
 export default MainHeader
